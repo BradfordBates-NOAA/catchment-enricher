9ac850fc49b451fb9b8d1a378e001fb86b27a698
Sophie Williams <scwilliams8@crimson.ua.edu>
2022-03-30 12:09:41 -0500

    levee stats counter added to pixel_counter_functions

--- src/pixel_counter_functions.py
+++ src/pixel_counter_functions.py
@@ -44,8 +44,13 @@ def get_nlcd_counts(feat, masked):
 
 
 def get_levee_counts(feat, masked):
-    pass
-    return
+
+    # Acquires information for table on each levee attribute per catchment
+    feature_stats = {
+        'HydroID': feat.GetField('HydroID'),
+        'TotalLeveePixels': int(masked.count()),
+    }
+    return feature_stats
 
 def get_bridge_counts(feat_masked):
     pass
